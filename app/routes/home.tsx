cad42971cf7bb0b1b41d232ee41ed7590dc03f6a
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2025-10-22 18:01:39 +0700

    add  shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'Jual Gas' },
+    { name: 'description', content: 'Welcome to Jual Gas!' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Helo World!</div>;
 }
